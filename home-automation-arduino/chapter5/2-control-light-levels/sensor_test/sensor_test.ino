fdf4c89dd8e8e3914186bc08a958690996cf1565
openhomeautomation <marco@Marc-Oliviers-MacBook-Pro.local>
2014-02-26 11:10:30 +0100

    small fixes

--- home-automation-arduino/chapter5/2-control-light-levels/sensor_test/sensor_test.ino
+++ home-automation-arduino/chapter5/2-control-light-levels/sensor_test/sensor_test.ino
@@ -1,17 +1,17 @@
-// Define analog pin
-int sensorPin = 1;
+// Define pins
+int switchPin = 2;
 
 // Setup
 void setup() {
     // Init serial
-    Serial.begin(9600);
+    Serial.begin(115200);
 }
 
 // Main loop
 void loop() {
     
     // Get temperature
-    int sensorValue = analogRead(sensorPin);
+    int sensorValue = digitalRead(sensorPin);
     
     // Put temperature on the serial port
     Serial.println(sensorValue,DEC);
